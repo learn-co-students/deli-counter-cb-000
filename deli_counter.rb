679551f919ae96736a112f459cbee8cfe37fa709
Cleavon Brathwaite <cbrathwaite90@gmail.com>
2017-03-25 08:40:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index {|x,index|puts "The line is currently:#{index+1}.#{x}"}
+  end
+
+end
