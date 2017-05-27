45fc4cf06ab37ee16ecb3d0e4beca7e2703abe70
Tony Nassif <tonynassif@gmail.com>
2017-05-27 22:59:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,9 @@ def line(guests)
     puts "The line is currently: " + guest_list
   end 
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  line_number = array.count
+  puts "Welcome, #{name}. You are number #{line_number} in line."
+end
