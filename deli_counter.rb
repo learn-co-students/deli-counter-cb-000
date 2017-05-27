35cdf4b026d429fc81146fb06bcc8c9d613be2d0
Tony Nassif <tonynassif@gmail.com>
2017-05-27 22:43:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(guests)
+  if guests.length === 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+    guests.each_with_index { |guest, index| new_array.push("#{index + 1}. #{guest}")}
+    puts new_array
+  end 
+end
