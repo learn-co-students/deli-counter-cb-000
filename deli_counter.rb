999cf520772740aea8d0ac8a60dcac0b1aee1f94
Tony Nassif <tonynassif@gmail.com>
2017-05-27 23:04:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,12 @@ def take_a_number(array, name)
   line_number = array.count
   puts "Welcome, #{name}. You are number #{line_number} in line."
 end
+
+def now_serving(line)
+  if line.length === 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift()
+  end
+end
