6e8f8e32b88d6079cb98dd2ca61039fab1af4260
Steve Cordova <cordovas@bc.edu>
2018-06-28 18:29:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  x = array.length
+  puts "Welcome, #{name}. You are number #{x} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    x = array.shift
+    puts "Currently serving #{x}."
+  end
+end
