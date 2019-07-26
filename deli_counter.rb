936abe4cfc9d94d4642f0bfc8af85d404e5c0f93
Will Hodgson <will.hodgson@current-rms.com>
2019-07-26 15:23:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
+
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    array.each do |element|
+      index = array.index(element)
+      number = index + 1
+      array[index] = "#{number}. #{element}"
+    end
+    puts "The line is currently: #{array.join(' ')}"
+  end
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+end
